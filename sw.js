e9e91bf83db6d23ff2fdf5e905ecde2d3e9784c7
Chirana Nimnaka <chirana.info@gmail.com>
2026-03-15 14:45:04 +0530

    Force Service Worker update to v3 and cache new assets

--- sw.js
+++ sw.js
@@ -1,15 +1,33 @@
-const CACHE_NAME = 'ss-v2';
+const CACHE_NAME = 'ss-v3';
 const ASSETS = [
   'index.html',
   'css/style.css',
   'js/mode-controller.js',
-  'favicon.png'
+  'favicon.png',
+  'assets/SportSphere.apk',
+  'assets/SportSphere_Windows.zip'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('SW: Pre-caching assets...');
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('SW: Removing old cache...', key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
