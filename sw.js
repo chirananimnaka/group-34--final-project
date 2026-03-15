6197c8bf46f8d480c7b3e56993e05acdfe899090
Chirana Nimnaka <chirana.info@gmail.com>
2026-03-15 13:31:18 +0530

    Convert site to PWA with install buttons and service worker

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ss-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/mode-controller.js',
+  '/favicon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
